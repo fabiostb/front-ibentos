f39773640005a7d129a475dbc1d8b395e6c7e3c0
Fabio Santiago <santiago.fabio@gmail.com>
2021-10-27 11:40:10 -0400

    Set API Url in the environment variable

--- src/app/ibento-service/ibento.service.ts
+++ src/app/ibento-service/ibento.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 
-import { Ibento } from './ibento';
+import {Ibento} from './ibento';
 import {IbentoList} from './ibento-list';
+import {environment} from '../../environments/environment';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class IbentoService {
 
-  constructor(private http: HttpClient) { }
-
-  private readonly API_URL = 'http://localhost:8080/api/ibento';
+  constructor(private http: HttpClient) {
+  }
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -24,7 +24,7 @@ export class IbentoService {
 
   /** GET ID */
   getIbento(id: string): Observable<Ibento> {
-    const url = `${this.API_URL}/${id}`;
+    const url = `${environment.API_URL}/${id}`;
 
     return this.http.get<Ibento>(url).pipe(
       tap(_ => IbentoService.log(`fetched ibento id=${id}`)),
@@ -34,7 +34,7 @@ export class IbentoService {
 
   /** GET ALL */
   getIbentos(): Observable<IbentoList[]> {
-    return this.http.get<IbentoList[]>(this.API_URL)
+    return this.http.get<IbentoList[]>(environment.API_URL)
       .pipe(
         tap(_ => IbentoService.log('fetched all ibentos')),
         catchError(this.handleError<IbentoList[]>('getIbentos', []))
@@ -47,17 +47,17 @@ export class IbentoService {
       return of([]);
     }
 
-    return this.http.get<Ibento[]>(`${this.API_URL}/find?name=${term}`).pipe(
+    return this.http.get<Ibento[]>(`${environment.API_URL}/find?name=${term}`).pipe(
       tap(x => x.length ?
-         IbentoService.log(`found ibentos matching "${term}"`) :
-         IbentoService.log(`no ibentos matching "${term}"`)),
+        IbentoService.log(`found ibentos matching "${term}"`) :
+        IbentoService.log(`no ibentos matching "${term}"`)),
       catchError(this.handleError<Ibento[]>('searchIbentos', []))
     );
   }
 
   /** POST */
   addIbento(ibento: Ibento): Observable<Ibento> {
-    return this.http.post<Ibento>(this.API_URL, ibento, this.httpOptions).pipe(
+    return this.http.post<Ibento>(environment.API_URL, ibento, this.httpOptions).pipe(
       tap((ibentoCreated: Ibento) => IbentoService.log(`added ibento id=${ibentoCreated.id}`)),
       catchError(this.handleError<Ibento>('updateIbento'))
     );
@@ -65,7 +65,7 @@ export class IbentoService {
 
   /** PUT */
   updateIbento(ibento: Ibento): Observable<Ibento> {
-    return this.http.put<Ibento>(this.API_URL, ibento, this.httpOptions).pipe(
+    return this.http.put<Ibento>(environment.API_URL, ibento, this.httpOptions).pipe(
       tap((ibentoUpdated: Ibento) => IbentoService.log(`updated ibento id=${ibentoUpdated.id}`)),
       catchError(this.handleError<Ibento>('updateIbento'))
     );
@@ -73,7 +73,7 @@ export class IbentoService {
 
   /** DELETE */
   deleteIbento(id: string): Observable<any> {
-    const url = `${this.API_URL}/${id}`;
+    const url = `${environment.API_URL}/${id}`;
 
     return this.http.delete(url, this.httpOptions).pipe(
       tap(_ => IbentoService.log(`deleted ibento id=${id}`)),
